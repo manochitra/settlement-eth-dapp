3ed912500b74d796ff6493f7281d727eafb12d5c
Manochitra S <manochitraselvam@gmail.com>
2018-05-22 07:02:52 +0100

    message

--- src/js/events.js
+++ src/js/events.js
@@ -1,40 +1,39 @@
 const express = require('express');
 const router = express.Router();
-require('babel-core/register');
-require('babel-polyfill');
 import { default as Web3} from "web3"
 import { default as contract } from "truffle-contract"
 import eventArtifacts from "../../build/contracts/SettlementEvents.json"
 var EventContract = contract(eventArtifacts)
-
-const SettlementEvents = artifacts.require("./SettlementEvents.sol")
+var eventData="";
 //var settlement = require('./app.js');
 router.post('/', (req, res, next) => {
-    const event = {
+    eventData = {
         windowId: req.body.windowId,
         eventid: req.body.eventid,
         currencycode: req.body.currencycode,
         tranamount: req.body.tranamount
     };
-    console.log(event);
+    console.log(eventData);
+   
     res.status(201).json({
         message: 'Event is pushed',
-        event: event
+        event: eventData
        
     });
+    settlementevent(eventData);
 });
 
-start : function() {
+
+
+ function settlementevent(eventData) {
+	 console.log("Entered! ");
+	 console.log(eventData);
 	EventContract.setProvider(window.web3.currentProvider)
     EventContract.defaults({from: window.web3.eth.accounts[0],gas:6721975})
-    EventContract.deployed().
-}
-
-event: function(eventData) {
-	  
     EventContract.deployed().then(function(instance){
       instance.addSettlementEvent("PARSER","10","Sucess").then(function(result){
-    	  console.log("Success! ")
+    	  console.log("Success! ");
+    	  return "Success"
       })
     }).catch(function(err){ 
       console.error("ERROR! " + err.message)
@@ -43,6 +42,3 @@ event: function(eventData) {
 
   
 module.exports = router;
-module.exports = function(deployer) {
-	deployer.deploy(SettlementEvents);
-};
--- src/js/restapp.js
+++ src/js/restapp.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+require('babel-core/register');
+require('babel-polyfill');
 const eventsRoutes = require("./events");
 
 
